f09525df15f10bae4fafc44835b3ab25c6f6762b
Austin Major <mr.austinmajor@gmail.com>
2019-02-26 16:18:23 -0600

    finished blackjack program

--- assignment_3/blackjack.cpp
+++ assignment_3/blackjack.cpp
@@ -3,78 +3,103 @@
 
 using namespace std;
 
-int simulateOneGame();
-bool checkGameStatus(int);
+/*
+* write a function that picks/generates the next card.
+* write a function to tell if the dealer is busted or not.
+* write a function to simulate the game for n times, where n is specified by user.
+*/
+
 int pickRandomCard();
+bool checkGameStatus(int);
+int simulateOneGame();
 
-//function to check dealer is busted or not
-bool checkGameStatus(int score)
-{
-  //Player is busted or not
-  return score >= 22 || score >= 17;
-}
+void simulate();
 
-//function to simulate game n times.
-void simulate(int n)
-{
-  int holds = 0;
-  int busts = 0;
-  for (int i = 0; i < n; i++)
-  {
-    //simulating ith game out of total n game.
-    int score = simulateOneGame();
-    //if score is more then 22 then busts then hold.
-    if (score >= 22)
-      busts += 1;
-    else
-      holds += 1;
-  }
-  cout << busts << " game out of total " << n << " games are busted" << endl;
-  cout << "Probability of bursts for given n = " << (float)busts / n << endl;
-}
+void printIntro();
+void getInputs();
+void printResult();
 
-//function to pick random card
+int n;
+int busts = 0;
+
+//picks a random card
 int pickRandomCard()
 {
   return rand() % ((12 + 1));
 }
 
-//function that perform one pass of the game
+//check if dealer busted or not
+bool checkGameStatus(int score)
+{
+  return score >= 22 || score >= 17;
+}
+
 int simulateOneGame()
 {
   int score = 0;
-  //THis is cardScore list contains the score of each card.
   int cardScore[13] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+
   while (checkGameStatus(score) == false)
   {
-    //picking random card;
     int temp = cardScore[pickRandomCard()];
 
     if (score >= 17)
       break;
+
     if (temp != 11)
     {
       score += temp;
     }
-    else if (temp == 11 and (score >= 6 and score <= 10))
+    else if (temp == 11 && (score >= 6 && score <= 10))
       score += 11;
     else
       score += 1;
   }
+
   return score;
+}
+
+//simulate game n times.
+void simulate()
+{
+  int holds = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    int score = simulateOneGame();
+    if (score >= 22)
+      busts += 1;
+    else
+      holds += 1;
+  }
+}
+
+void printIntro()
+{
+  cout << "John Zelle's Blackjack Game" << endl;
+  cout << "How many games to simulate: ";
+}
+
+void getInputs()
+{
+  cin >> n;
+  cout << endl;
+}
+
+void printResult()
+{
+  cout << "Total # of games: " << n << endl;
+  cout << "User Wins: " << busts << endl;
+  cout << "Dealer Wins: " << n - busts << endl;
+  cout << "Probability Dealer will bust: " << (float)busts / n << endl;
 }
 
 int main()
 {
-  //Introducing game
-  cout << "This code will simulate the Blackjack game" << endl;
+  printIntro();
+  getInputs();
+  simulate();
+  printResult();
 
-  //taking inputs
-  int n;
-  cout << "Enter the number of games you want to simulate: " << endl;
-  cin >> n;
-
-  //simulating one game. This task will be implemented as separate function.
-  simulate(n);
   return 0;
 }
